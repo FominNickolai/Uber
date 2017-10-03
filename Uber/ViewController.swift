491154de92b4a622a115f2e11c88ec3385fb214a
Fomin Nickolai <giraffe.in.ua@gmail.com>
2017-10-03 17:45:05 +0300

    Add methods to Create User in Firebase or Login if user is already exist.

--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -32,7 +32,27 @@ class ViewController: UIViewController {
     //MARK: - @IBActions
     @IBAction func topTapped(_ sender: UIButton) {
         
-        
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            
+            displayAlertController(withTitle: "Missing Information", andMessage: "You must provide both a email and password")
+            
+        } else {
+            
+            guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+            
+            if signUpMode {
+                //Sign Up
+    
+                createUserInFirebase(withEmail: email, andPassword: password)
+                
+            } else {
+                //Log In
+                
+                signInToFirebase(withEmail: email, andPassword: password)
+                
+            }
+            
+        }
         
     }
     
@@ -62,6 +82,65 @@ class ViewController: UIViewController {
             signUpMode = true
             
         }
+        
+    }
+    
+    /// Display Alert to the User if any error.
+    ///
+    /// - Parameters:
+    ///   - title: Alert Title
+    ///   - message: Alert Message
+    func displayAlertController(withTitle title: String, andMessage message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    /// Create User in Firebase
+    ///
+    /// - Parameters:
+    ///   - email: User email
+    ///   - password: User password
+    func createUserInFirebase(withEmail email: String, andPassword password: String) {
+        
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+            
+            if let error = error {
+                
+                self.displayAlertController(withTitle: "Error", andMessage: error.localizedDescription)
+                
+            } else {
+                
+                print("Sign Up Success")
+                
+            }
+            
+        })
+        
+    }
+    
+    /// Sign In to Firebase with email and password
+    ///
+    /// - Parameters:
+    ///   - email: User email
+    ///   - password: User password
+    func signInToFirebase(withEmail email: String, andPassword password: String) {
+        
+        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+            
+            if let error = error {
+                
+                self.displayAlertController(withTitle: "Error", andMessage: error.localizedDescription)
+                
+            } else {
+                
+                print("Sign In Success")
+                
+            }
+            
+        })
         
     }
 }
